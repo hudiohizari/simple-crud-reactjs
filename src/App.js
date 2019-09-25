85578bd8f26a01beb7eab89390a8098f7ae3c4f1
Hudio Hizari <hhizari@gmail.com>
2019-09-26 02:27:18 +0700

    Add delete function

--- src/App.js
+++ src/App.js
@@ -1,26 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+import ProductItem from './Model/ProductItem.js'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const products = [
+  {
+    name: 'iPhone 7',
+    price: 600
+  },
+  {
+    name: 'iPhone 6',
+    price: 200
+  }
+]
+localStorage.setItem('products', JSON.stringify(products))
+
+class App extends Component {
+  constructor(props){
+    super(props)
+
+    this.state = {
+      products: JSON.parse(localStorage.getItem('products'))
+    }
+
+    this.onDelete = this.onDelete.bind(this)
+  }
+
+  componentDidMount(){
+    const products = this.getProducts()
+    this.setState({ products })
+  }
+
+  getProducts(){
+    return this.state.products
+  }
+
+  onDelete(name){
+    const products = this.getProducts()
+    const filteredProduct = products.filter(product => {
+      return product.name !== name
+    })
+
+    this.setState({ products: filteredProduct })
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <h1>Product Manager</h1>
+        {
+          this.state.products.map(product => {
+            return (
+              <ProductItem
+                key = {product.name}
+                {...product}
+                onDelete = {this.onDelete}
+              />
+            )
+          })
+        }
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
